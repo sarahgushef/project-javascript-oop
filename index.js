5451f6e1288352d36b3cd855c620b4874b8f5b9b
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-23 10:37:52 +0700

    fix the grammar in function

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-class Foods {
+class Food {
   constructor(name = "Nasi Goreng", color = "orange") {
     this.name = name
     this.color = color
   }
 
-  cooked() {
+  cook() {
     console.log(`${this.name} is being cooked`)
   }
 }
 
-const sayurAsem = new Foods("Sayur Asem", "orange")
+const sayurAsem = new Food("Sayur Asem", "orange")
 console.log(sayurAsem)
 
-sayurAsem.cooked()
+sayurAsem.cook()
 
 // =============================================== extends (inheritence)
 
-class FavoriteFoods extends Foods {
-  constructor({ name, color, price, origin }) {
+class FavoriteFood extends Food {
+  constructor({ name, color, price, origin, ingredients }) {
     super(name, color)
     this.price = price
     this.origin = origin
+    this.ingredients = ingredients
   }
 
   getPrice() {
@@ -40,13 +41,16 @@ class FavoriteFoods extends Foods {
 
 // ============================== new Object from inheritence
 
-const myFavoriteFood = new FavoriteFoods({
+const myFavoriteFood = new FavoriteFood({
   name: "Sate Ayam",
   color: "brown",
   price: 30000,
-  origin: "Indonesia"
+  origin: "Indonesia",
+  ingredients: ["ayam", "kecap", "bawang merah", "saus kacang"]
 })
 
 console.log(myFavoriteFood)
 console.log(myFavoriteFood.name)
 console.log(myFavoriteFood.getOrigin())
+console.log(myFavoriteFood.ingredients)
+console.log(myFavoriteFood.getPrice())
